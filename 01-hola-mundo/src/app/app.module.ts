35b8358887a958359eee675b3fcf6933c1a2fc1a
GuillermoCT <squall_600@hotmail.com>
2021-10-19 12:02:59 +0200

    Pruebas

--- 01-hola-mundo/src/app/app.module.ts
+++ 01-hola-mundo/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import {HeaderComponent} from './components/header/header.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent,HeaderComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ 01-hola-mundo/src/app/components/header/header.component.ts
@@ -0,0 +1,12 @@
+
+import {Component} from '@angular/core'
+
+@Component({
+
+    selector: 'app-header',
+    template: `<h1>Header component</h1>`
+})
+
+export class HeaderComponent{
+
+}
